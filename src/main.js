90bcc12a2a5700693f95f9b41dae59ee4ff2a739
Mattes Groeger <info@mattes-groeger.de>
2016-03-11 02:35:20 +0100

    Add amplitude tracking

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import { render } from 'react-dom'
 
 import Router from './routes';
 
+if (process.env.ENV === 'production') {
+  amplitude.init("b394c3c7237d0356f3f2f2a0f1802d4f")
+}
+//  else {
+//   amplitude.init("2931ae77475ddfe3affa319a2bffbdbe")
+// }
+
+amplitude.logEvent('startup')
+
 render(Router, document.getElementById('app'))
