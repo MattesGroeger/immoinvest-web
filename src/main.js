e38d0a0c595f8e1e7f6a82dd150fec1fee7820b2
Mattes Groeger <info@mattes-groeger.de>
2016-03-11 11:46:20 +0100

    Only track on production

--- src/main.js
+++ src/main.js
@@ -5,11 +5,10 @@ import Router from './routes';
 
 if (process.env.ENV === 'production') {
   amplitude.init("b394c3c7237d0356f3f2f2a0f1802d4f")
+  amplitude.logEvent('startup')
 }
 //  else {
 //   amplitude.init("2931ae77475ddfe3affa319a2bffbdbe")
 // }
-
-amplitude.logEvent('startup')
 
 render(Router, document.getElementById('app'))
